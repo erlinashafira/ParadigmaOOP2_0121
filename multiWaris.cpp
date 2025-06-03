108f88b17ff9ab5790a6a89e0ad5f901c0e0ac89
errerew <erlina31acc@gmail.com>
2025-06-03 18:23:53 +0700

    menambahkan member dari orang

--- multiWaris.cpp
+++ multiWaris.cpp
@@ -1,6 +1,12 @@
-#include <iostram>
+#include <iostream>
 using namespace std;
 
 class orang{
-    
-}
+    public:
+        int umur;
+        orang(int pUmur) :
+            umur (pUmur)
+        {
+            cout << "orang dibuat dengan umur" << umur << "\n" << endl;
+        }
+};
